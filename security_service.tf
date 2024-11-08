ce0f8eee4482ef5d987c22096e79dabf0d440cd4
CarolGuay <140135315+CarolGuay@users.noreply.github.com>
2024-11-08 00:32:55 -0600

    Update security_service.tf

--- security_service.tf
+++ security_service.tf
@@ -1,22 +1,27 @@
-# Definir el grupo de recursos de Inspector  
-resource "aws_inspector_resource_group" "my_resource_group" {  
-  tags = {  
-    Name = "MyResourceGroup"  
-  }  
-}  
-
-# Definir la plantilla de evaluación de Inspector  
-resource "aws_inspector_assessment_template" "assessment_template" {  
-  name               = "MyAssessmentTemplate"  
-  duration           = 60  
-  target_resource_ids = [aws_inspector_resource_group.my_resource_group.arn]  
-  rules_package_arns  = [  
-    "arn:aws:inspector:us-east-1:123456789012:rulespackage/0-9hgA8AJr" # Reemplaza con el ARN de tu paquete de reglas real  
-  ]  
-}  
-
-# Definir el bucket S3 para el análisis de datos  
 resource "aws_s3_bucket" "data_analysis_datos" {  
   bucket = "data-analysis-datos"  
   acl    = "private"  
+}  
+
+resource "aws_s3_bucket_acl" "data_analysis_datos_acl" {  
+  bucket = aws_s3_bucket.data_analysis_datos.id  
+  acl    = "private"  
+}  
+
+resource "aws_inspector_resource_group" "my_resource_group" {  
+  tags = {  
+    Name = "My Resource Group"  
+  }  
+}  
+
+resource "aws_inspector_assessment_template" "assessment_template" {  
+  name       = "my-assessment-template"  
+  target_arn = aws_inspector_resource_group.my_resource_group.arn  
+  duration   = 3600  
+  rules_package_arns = [  
+    "arn:aws:inspector:us-east-1:inspector:rules-package/0-9hgA48vF",  
+    "arn:aws:inspector:us-east-1:inspector:rules-package/0-R01qwB5Q",  
+    "arn:aws:inspector:us-east-1:inspector:rules-package/0-L3Z9ybkY",  
+    "arn:aws:inspector:us-east-1:inspector:rules-package/0-PmNV0Tcd",  
+  ]  
 }
