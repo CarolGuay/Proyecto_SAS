b2e478f68f8505e1c9c1c94f15aca541b6db5d68
CarolGuay <140135315+CarolGuay@users.noreply.github.com>
2024-11-08 00:25:33 -0600

    Update security_service.tf

--- security_service.tf
+++ security_service.tf
@@ -1,14 +1,22 @@
+# Definir el grupo de recursos de Inspector  
 resource "aws_inspector_resource_group" "my_resource_group" {  
-  tags = {  # Reemplaza resource_group_tags con tags  
+  tags = {  
     Name = "MyResourceGroup"  
   }  
 }  
 
+# Definir la plantilla de evaluación de Inspector  
 resource "aws_inspector_assessment_template" "assessment_template" {  
-  name                = "MyAssessmentTemplate"  
-  duration            = 60  
-  target_arn         = aws_inspector_resource_group.my_resource_group.arn  
+  name               = "MyAssessmentTemplate"  
+  duration           = 60  
+  target_resource_ids = [aws_inspector_resource_group.my_resource_group.arn]  
   rules_package_arns  = [  
-    "arn:aws:s3:::data-analisis-datos"  # Reemplaza con tu ARN de paquete de reglas real  
+    "arn:aws:inspector:us-east-1:123456789012:rulespackage/0-9hgA8AJr" # Reemplaza con el ARN de tu paquete de reglas real  
   ]  
 }  
+
+# Definir el bucket S3 para el análisis de datos  
+resource "aws_s3_bucket" "data_analysis_datos" {  
+  bucket = "data-analysis-datos"  
+  acl    = "private"  
+}
